0ea1bb5a5dc5b7b993c38aa1c9a9f12148487042
Shivam <kumarshivam3788@gmail.com>
2025-03-30 01:59:10 +0530

    Add Q25 class to find the largest number from user-input until zero is entered and close scanner

---
+++ Conditionals and Loops/Assignment/Q25.java
@@ -0,0 +1,22 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class Q25 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the number : ");
+        int largest = 0;
+
+        while (true) {
+            int num = in.nextInt();
+            if (num == 0) {
+                break;
+            }
+            if (largest < num) {
+                largest = num;
+            }
+        }
+        System.out.println("Largest is " + largest);
+    }   
+}
