bfc03dd76a5c040bbd4a659b84f086aa56d3a852
Shivam <kumarshivam3788@gmail.com>
2025-03-29 22:03:15 +0530

    Add Q8 class to check if a string is a palindrome based on user input

---
+++ Basics/Assignment/Q8.java
@@ -0,0 +1,31 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class Q8 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the string : ");
+
+        String str = in.next();
+
+        int left = 0;
+        int right = str.length() - 1;
+        boolean result = true;
+
+        while (left < right) {
+            if (str.charAt(left) != str.charAt(right)) {
+                result = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        if (result) {
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
+    }
+}
