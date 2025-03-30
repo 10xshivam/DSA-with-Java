1bd4e95834c9ee7bbbf249e502f7c1c845d3df48
Shivam <kumarshivam3788@gmail.com>
2025-03-31 01:07:35 +0530

    Add IQ23 class to check if a user-input year is a leap year and close scanner

---
+++ Conditionals and Loops/Assignment/IQ23.java
@@ -0,0 +1,17 @@
+package Assignment;
+
+import java.util.Scanner;
+
+public class IQ23 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the year : ");
+        int year = in.nextInt();
+
+        if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)){
+            System.out.println("Leap year.");
+        } else {
+            System.out.println("Not Leap year.");
+        }
+    }
+}
