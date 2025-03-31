42805c0f4e37fea5f4d3e88e4594ec2abe249787
Shivam <kumarshivam3788@gmail.com>
2025-03-31 14:06:14 +0530

    Add Q9 class to calculate the factorial of a user-input number; close scanner

---
+++ Methods/Assignment/Q9.java
@@ -0,0 +1,32 @@
+package Methods.Assignment;
+
+import java.util.Scanner;
+
+public class Q9 {
+    static void factorial(int n){
+        if(n <= 1){
+            System.out.println(n + "! = 1");
+            return;
+        }
+        
+        System.out.print(n + "! = ");
+        
+        int factorial = 1;
+
+        for (int i = n; i >= 1; i--) {
+            System.out.print(i);
+            if (i > 1) {
+                System.out.print(" * ");
+            }
+            factorial *= i;
+        }
+        System.out.println(" = " + factorial);
+    }
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter the number : ");
+        int num = in.nextInt();
+        factorial(num);
+        in.close();
+    }
+}
