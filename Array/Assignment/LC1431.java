4a349b4a393dc303b0a35bd56a9d19626e5d4392
Shivam <kumarshivam3788@gmail.com>
2025-04-05 22:28:03 +0530

    Add LC1431 class to determine which kids can have the greatest number of candies after receiving extra candies

---
+++ Array/Assignment/LC1431.java
@@ -0,0 +1,29 @@
+package Array.Assignment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LC1431 {
+    static public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> list = new ArrayList<>();
+        for(int i = 0; i < candies.length; i++){
+            int newCandies = candies[i] + extraCandies;
+            System.out.println(newCandies);
+            boolean result = true;
+            for(int j = 0; j < candies.length; j++){
+                if(newCandies < candies[j]){
+                    result = false;
+                }
+            }
+            System.out.println(result);
+            list.add(result);
+        }
+        return list;
+    }
+
+    public static void main(String[] args) {
+        int[] candies = {2,3,5,1,3};
+        int extraCandies = 3;
+        System.out.println(kidsWithCandies(candies, extraCandies));
+    }
+}
