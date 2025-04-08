b6e5bb942d2f703bc8977e3f8095cd953bef428b
Shivam <kumarshivam3788@gmail.com>
2025-04-08 22:37:11 +0530

    Add LC1389 class to implement functionality for creating a target array

---
+++ Array/Assignment/LC1389.java
@@ -0,0 +1,18 @@
+package Array.Assignment;
+import java.util.ArrayList;
+
+public class LC1389 {
+    public int[] createTargetArray(int[] nums, int[] index) {
+        ArrayList<Integer> a = new ArrayList<Integer>();
+        for(int i=0;i<nums.length;i++)
+        {
+            a.add(index[i],nums[i]);
+        }
+        int target[] = new int[nums.length];
+        for(int i=0;i<nums.length;i++)
+        {
+            target[i] = a.get(i);
+        }
+        return target;
+    }
+}
