1053bebbf14a99854cbe029b73eee6b10a5de741
10xshivam <kumarshivam3788@gmail.com>
2025-04-18 01:44:30 +0530

    Add LC1773 class to implement a method for counting matches based on specified rules

---
+++ Array/Assignment/LC1773.java
@@ -0,0 +1,23 @@
+package Array.Assignment;
+
+import java.util.List;
+
+public class LC1773 {
+    public int countMatches(List<List<String>> items, String ruleKey, String ruleValue) {
+        int index = 0;
+        if(ruleKey.equals("color")){
+            index = 1;
+        }
+        else if(ruleKey.equals("name")){
+            index = 2;
+        }
+
+        int count = 0;
+        for(List<String> item : items){
+            if(item.get(index).equals(ruleValue)){
+                count++;
+            }     
+        }
+        return count;
+    }
+}
