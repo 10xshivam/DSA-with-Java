224f83626009d98a64a090b92e532591c297d298
10xshivam <kumarshivam3788@gmail.com>
2025-04-23 20:56:03 +0530

    Refactor Star class to use a variable for the number of rows in the pattern

--- Patterns/Star.java
+++ Patterns/Star.java
@@ -2,7 +2,8 @@ package Patterns;
 
 public class Star {
     public static void main(String[] args) {
-        for (int i = 1; i <= 4; i++) {
+        int n = 4;
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
                 System.out.print("*");
             }
